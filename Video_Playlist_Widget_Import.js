6d01612591400ca6c691c21eae77f104dec10d4f
Chris Paschall <ChrisP1108@gmail.com>
2022-12-28 18:06:45 -0700

    Roboto Font CSS Import Added

--- Video_Playlist_Widget_Import.js
+++ Video_Playlist_Widget_Import.js
@@ -164,6 +164,7 @@ function initializeVideoPlaylist(inputData, elementRoot) { // ALL VIDEO PLAYLIST
   // CSS Generation 
 
     const playlistStyling = `
+      @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
       ${widgetIdSelector} > *,
       [data-lightboxid="${widgetIdSelector}"] > * {
         box-sizing: border-box;
